6adf098636eced838086b91a87bbcc2fb101fc14
ZiRaOmega <max.diet@free.fr>
2022-12-28 16:26:28 +0100

    Bash script that create files and folder for a webserver in golang

---
+++ Init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create the necessary directories
+mkdir -p static
+mkdir -p templates
+
+# Create an empty main.go file
+touch main.go
+
+# Create an empty index.html file in the templates directory
+touch templates/index.html
+
+# Create an empty CSS file in the static directory
+touch static/style.css
+
+# Create an empty JS file in the static directory
+touch static/script.js
